c2e9628d395a7507c898f4f7f5f99479737080bb
pinakin.chaubal <pinakin.chaubal@N10067.intellectdesign.com>
2021-05-13 17:38:28 +0530

    New updates

--- src/main/java/studentservice/controllers/StudentServiceContoller.java
+++ src/main/java/studentservice/controllers/StudentServiceContoller.java
@@ -1,5 +1,7 @@
 package studentservice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/students")
 public class StudentServiceContoller {
+	@Autowired
+	private Environment env;
 	
 	@GetMapping("/status/check")
 	public String status() {
-		return "working";
+		return "working on port "+env.getProperty("local.server.port");
 	}
 
 }
